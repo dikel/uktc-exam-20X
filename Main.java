b162af99e06099eee4a9c0e8a3b08c3a8f666c1b
rambi21 <rpavlov476@gmail.com>
2024-11-21 13:44:26 +0200

    promenen clas

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        Mouse mouse = new Mouse;
-        mouse.squeak();
+        Book book = new book;
+        book.squeak();
     }
 
-    static class Mouse {
+    static class Book {
         squeak() {
             System.println("Squeak");
         }
