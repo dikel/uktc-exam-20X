f3636be8eb3438dac6fd67767b3a829e0ab17dea
Ivan20318 <20318@uktc-bg.com>
2024-11-21 12:56:51 +0200

    I added parentheses , void return type, and corrected the print statement.

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        Mouse mouse = new Mouse;
-        mouse.squeak();
+        Mouse mouse = new Mouse(); 
+        mouse.squeak(); 
     }
 
     static class Mouse {
-        squeak() {
-            System.println("Squeak");
+       
+        void squeak() {
+            System.out.println("Squeak"); 
         }
     }
 }
