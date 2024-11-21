61a085164b207b8ea02faaeed02cf533611d34d2
IvayloMoev <ivaylomoev@gmail.com>
2024-11-21 11:54:43 +0200

    Fixed the java file Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        cat = new Cat();
+       Cat cat = new Cat();
         cat.meow();
     }
 
@@ -10,4 +10,16 @@ public class Main {
             System.out.println("Meow");
         }
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
