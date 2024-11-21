1a0dd457662cf755524e699e6311945240270e1c
docho12 <124052866+docho12@users.noreply.github.com>
2024-11-21 09:59:07 +0200

    updatedmain

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog;
-        dog.bark();
+        Dog dog = new Dog(); // Инициализиране на обекта
+        dog.bark();          // Извикване на метода bark
     }
 
     static class Dog {
-        bark() {
-            println("Bark");
+        void bark() { // Добавяне на модификатор и тип void
+            System.out.println("Bark"); // Правилно извикване на println
         }
     }
 }
