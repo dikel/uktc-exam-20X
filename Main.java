97eeab8b151b2784b284bfc1d7779b36e9a817d3
antoniovidov <91030382+antoniovidov@users.noreply.github.com>
2024-11-21 10:43:08 +0200

    Added new functionallity
    Added "jump" method to class Dog

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Dog dog;
         dog.bark();
+        dog.jump();
     }
 
     static class Dog {
         bark() {
             println("Bark");
         }
+        jump(){println("Jump");}
     }
 }
