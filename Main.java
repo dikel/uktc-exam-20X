50dc0d6600acbc43e3b82f4f6e26727e3d8f0739
SlaviAleksiev <20424@uktc-bg.com>
2024-11-21 13:42:56 +0200

    Added name and course to README.md

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        Mouse mouse = new Mouse;
+        Mouse mouse = new Mouse();
         mouse.squeak();
     }
 
     static class Mouse {
-        squeak() {
-            System.println("Squeak");
+        void squeak() {
+            System.out.println("Squeak");
         }
     }
 }
