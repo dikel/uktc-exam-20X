c810ad05226044ae5967eb187121899674e9a13c
Smashi203 <20302@uktc-bg.com>
2024-11-28 10:50:19 +0200

    Dobavqne na kursov nomer i ime

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
-public class Main {
+class Main {
 
     public static void main(String[] args) {
-        Dog dog;
+        Dog dog=new Dog();
         dog.bark();
     }
 
     static class Dog {
-        bark() {
-            println("Bark");
+        static void bark() {
+            System.out.println("Bark");
         }
     }
 }
