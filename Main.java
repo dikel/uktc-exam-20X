927b39e5acd42859b533cbb1b5aabe96a095fcf6
nattymi <nataliqnataliq06@gmail.com>
2024-12-04 22:37:01 +0200

    Fix code naj nakraq

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
-Natty is perfect
+ public static void main(String[] args) {
+        Dog dog;
+        dog.bark();
+
+        Cat cat;
+        cat.meow();
+    }
+
+    static class Dog {
+        bark() {
+            println("Tom");
+        }
+    }
+    static class Cat {
+        meow() {
+            println("Meow");
+        }
+    }
+}
