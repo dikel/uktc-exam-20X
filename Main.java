b13d212265a1df3cb566302e55150bb31a7e6d36
Ziky <20330@uktc-bg.com>
2024-12-04 19:22:36 +0200

    Exam 20330 (#1)
    * initial-203-commit
    
    * Добавяне на име и курсов номер
    
    * Поправка на кода
    
    ---------
    
    Co-authored-by: dikel <dikelito@tutamail.com>

---
+++ Main.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+Dog dog;
+public static void main(String[] args) {
+static class Dog {
+bark() {
+println("Bark");
+static void bark() {
+System.out.print("Bark");
+      }
+   }
+}
